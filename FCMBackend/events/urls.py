36ebc54fea0cb78e1b61d9d0cba8e4c5bcd794ee
Nataniel Chmielarz <129111960+NatanielChmielarz@users.noreply.github.com>
2024-06-10 20:28:47 +0200

    commit

--- FCMBackend/events/urls.py
+++ FCMBackend/events/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path,include
-from events.views import EventsListCreateView , EventsDeteilsView
+from events.views import EventsListCreateView, EventsRetrieveUpdateDestroy, mark_as_completed
 
 urlpatterns = [
      path('',EventsListCreateView.as_view()),
-     path("<int:id>/",EventsDeteilsView.as_view()),
+        path('<int:pk>/', EventsRetrieveUpdateDestroy.as_view(), name='event-detail'),
+    path('<int:pk>/mark_as_completed/', mark_as_completed, name='event-mark-as-completed'),
 
      
      
--- FCMBackend/events/views.py
+++ FCMBackend/events/views.py
@@ -1,30 +1,44 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
 from core.models import Events
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import Response
 from core.api.serializers import EventsSerializer
 from core.api.permision import UserOrReadOnly
-# Create your views here.
+from rest_framework.decorators import api_view
+
 class EventsListCreateView(generics.ListCreateAPIView):
     serializer_class = EventsSerializer
     permission_classes = [UserOrReadOnly,IsAuthenticated]
     
     def get_queryset(self):
         id = self.request.user.id
-        return Events.objects.filter(user=id)
+        is_active = self.request.query_params.get('is_active')
+        if is_active is not None:
+            queryset = Events.objects.filter(is_active=is_active.lower() == 'true')
+        
+        return queryset.filter(user=id)
     
     def list(self, request, *args, **kwargs):
         queryset = self.get_queryset()
-        data = [{'title': note.title, 'id': note.id} for note in queryset]
+        data = [{'title': event.title, 'id': event.id} for event in queryset]
         return Response(data)
     
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
         
         
-class EventsDeteilsView(generics.RetrieveUpdateDestroyAPIView):
+class EventsRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Events.objects.all()
-    serializer_class = EventsSerializer
-    lookup_field='id'
-    permission_classes = [UserOrReadOnly,IsAuthenticated] 
+    serializer = EventsSerializer
+    permission_classes=[UserOrReadOnly,IsAuthenticated]
+@api_view(['POST'])
+def mark_as_completed(request, pk):
+    try:
+        event = Events.objects.get(pk=pk)
+    except Events.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    event.is_active = False
+    event.save()
+    return Response(status=status.HTTP_200_OK)
