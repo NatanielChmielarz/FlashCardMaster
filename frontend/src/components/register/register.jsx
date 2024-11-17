cf2a47cf98deee9d5b6343d22372e3893e44bf6b
Nataniel Chmielarz <129111960+NatanielChmielarz@users.noreply.github.com>
2024-11-17 20:42:53 +0100

    commit

--- frontend/src/components/register/register.jsx
+++ frontend/src/components/register/register.jsx
@@ -1,22 +1,23 @@
-import { useState } from 'react';
-import "./register.scss"
-import Header from '../Header';
-import axios from 'axios';
+import { useState } from "react";
+import "./register.scss";
+import Header from "../Header";
+import axios from "axios";
 import { NavLink } from "react-router-dom";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import AuthLayout from "../authlayout/authlayout";
 export default function Register() {
   const [submitted, setSubmitted] = useState(false);
-  const navigate = useNavigate();;
+  const navigate = useNavigate();
   const [errors, setErrors] = useState({});
   const [data, setData] = useState({
-    username: '',
-    email: '',
-    password: '',
-    confirm_password: '',
+    username: "",
+    email: "",
+    password: "",
+    confirm_password: "",
   });
 
   function handleInputChange(identifier, value) {
-    setData(prevData => ({
+    setData((prevData) => ({
       ...prevData,
       [identifier]: value,
     }));
@@ -25,19 +26,20 @@ export default function Register() {
   function handleRegister() {
     let newErrors = {};
 
-    if (!data.email.includes('@')) {
-      newErrors.email = 'Enter a valid email address';
+    if (!data.email.includes("@")) {
+      newErrors.email = "Enter a valid email address";
     }
     if (data.password.length < 8) {
-      newErrors.password = 'Password must be at least 8 characters';
+      newErrors.password = "Password must be at least 8 characters";
     } else if (!/[A-Z]/.test(data.password)) {
-      newErrors.password = 'Password must have at least one alphanumeric character';
+      newErrors.password =
+        "Password must have at least one alphanumeric character";
     } else if (!/[!@#$%^&*(),.?":{}|<>]/.test(data.password)) {
-      newErrors.password = 'Password must have at least one special symbol';
+      newErrors.password = "Password must have at least one special symbol";
     }
 
     if (data.password !== data.confirm_password) {
-      newErrors.confirm_password = 'Password didnt match';
+      newErrors.confirm_password = "Password didnt match";
     }
 
     if (Object.keys(newErrors).length > 0) {
@@ -45,79 +47,96 @@ export default function Register() {
     } else {
       setSubmitted(true);
       setErrors({});
-      axios.post('https://flash-card-master-backend.vercel.app/account/register/', {
-        username: data.username,
-        email: data.email,
-        password: data.password,
-        confirm_password:data.confirm_password,
-      })
-      .then(response => {
-        console.log('Rejestracja udana:', response.data);
-        navigate('/');
-        
-      })
-      .catch(error => {
-        console.error('Błąd rejestracji:', error);
-        // Tutaj możesz dodać obsługę błędów
-      });
+      axios
+        .post(
+          "https://flash-card-master-backend.vercel.app/account/register/",
+          {
+            username: data.username,
+            email: data.email,
+            password: data.password,
+            confirm_password: data.confirm_password,
+          }
+        )
+        .then((response) => {
+          console.log("Rejestracja udana:", response.data);
+          navigate("/");
+        })
+        .catch((error) => {
+          console.error("Błąd rejestracji:", error);
+          // Tutaj możesz dodać obsługę błędów
+        });
     }
   }
+  const rightContent = (
+    <div className="auth-inputs">
+      <h1>Create Account</h1>
+      <div className="controls">
+        <input
+          placeholder="Email"
+          type="email"
+          className={errors.email ? "invalid input-field" : "input-field"}
+          value={data.email}
+          onChange={(event) => handleInputChange("email", event.target.value)}
+        />
+        {errors.email && <span className="error">{errors.email}</span>}
 
+        <input
+          placeholder="Username"
+          className="input-field"
+          type="text"
+          value={data.username}
+          onChange={(event) =>
+            handleInputChange("username", event.target.value)
+          }
+        />
+
+        <input
+          placeholder="Password"
+          type="password"
+          className={errors.password ? "invalid input-field" : "input-field"}
+          value={data.password}
+          onChange={(event) =>
+            handleInputChange("password", event.target.value)
+          }
+        />
+        {errors.password && <span className="error">{errors.password}</span>}
+
+        <input
+          placeholder="Confirm password"
+          type="password"
+          className={
+            errors.confirm_password ? "invalid input-field" : "input-field"
+          }
+          value={data.confirm_password}
+          onChange={(event) =>
+            handleInputChange("confirm_password", event.target.value)
+          }
+        />
+        {errors.confirm_password && (
+          <span className="error">{errors.confirm_password}</span>
+        )}
+
+        <button className="btn-signup" onClick={handleRegister}>
+          Register
+        </button>
+      </div>
+    </div>
+  );
+  const leftcontent = (
+    <div className="welcome">
+      <h1>Welcome Back</h1>
+      <p>To keep connect with up please loin with your personal info </p>
+      <div className="login_button">
+        <NavLink to={"/login"} className="btn-signin">
+          SIGN UP
+        </NavLink>
+      </div>
+    </div>
+  );
   return (
     <>
-    <Header/>
-    <div id="auth-inputs">
-      <div className="controls">
-        <p>
-          <label>Email</label>
-          <input
-            type="email"
-            className={errors.email ? 'invalid' : undefined}
-            value={data.email}
-            onChange={(event) => handleInputChange('email', event.target.value)}
-          />
-          {errors.email && <span className="error">{errors.email}</span>}
-        </p>
-        <p>
-          <label>Username</label>
-          <input
-            type="text"
-            value={data.username}
-            onChange={(event) => handleInputChange('username', event.target.value)}
-          />
-        </p>
-        <p>
-          <label>Password</label>
-          <input
-            type="password"
-            className={errors.password ? 'invalid' : undefined}
-            value={data.password}
-            onChange={(event) =>
-              handleInputChange('password', event.target.value)
-            }
-          />
-          {errors.password && <span className="error">{errors.password}</span>}
-        </p>
-        <p>
-          <label>Confirm password</label>
-          <input
-            type="password"
-            className={errors.confirm_password ? 'invalid' : undefined}
-            value={data.confirm_password}
-            onChange={(event) =>
-              handleInputChange('confirm_password', event.target.value)
-            }
-          />
-          {errors.confirm_password && <span className="error">{errors.confirm_password}</span>}
-        </p>
-      </div>
-      <div className="actions">
-      <button className='button' onClick={handleRegister}>Register</button>
-        <NavLink to={'/login'} type="button" className="text-button">
-          You have an account? Just Login!
-        </NavLink>
-        
-      </div>
-    </div></>
+      <Header />
+      <AuthLayout leftContent={leftcontent} rightContent={rightContent} />
+    </>
   );
 }
